438ffa475eaac10629d59dd7709d73af7e43fb9d
OneCodeMonkey <1460018362@qq.com>
2022-02-14 17:30:17 +0800

    feat

---
+++ spring-full-features/Impl/src/main/java/com/liuyang1/impl/ImplApplication.java
@@ -0,0 +1,14 @@
+package com.liuyang1.impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.liuyang1"})
+public class ImplApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        SpringApplication.run(ImplApplication.class, args);
+    }
+
+}
---
+++ spring-full-features/Impl/src/test/java/com/liuyang1/impl/ImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuyang1.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
