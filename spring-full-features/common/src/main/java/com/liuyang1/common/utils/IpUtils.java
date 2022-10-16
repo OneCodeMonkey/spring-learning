08340c0240b84ce16d1faa8f887d346680e652aa
OneCodeMonkey <1460018362@qq.com>
2022-10-16 20:06:28 +0800

    modify

--- spring-full-features/common/src/main/java/com/liuyang1/common/utils/IpUtils.java
+++ spring-full-features/common/src/main/java/com/liuyang1/common/utils/IpUtils.java
@@ -48,23 +48,3 @@ public class IpUtils {
         return ipAddress;
     }
 }
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
