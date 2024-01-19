2687c61003ee59bc38435fe6111f170ebf57faff
OneCodeMonkey <1460018362@qq.com>
2024-01-19 20:41:20 +0800

    modify

--- redis-full/src/test/java/com/liuyang1/redisfull/ZSetOperationTest.java
+++ redis-full/src/test/java/com/liuyang1/redisfull/ZSetOperationTest.java
@@ -1,10 +1,47 @@
 package com.liuyang1.redisfull;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
 import redis.clients.jedis.Jedis;
 
 // todo
+@Slf4j
 public class ZSetOperationTest extends RedisBasicTest {
+    @Test
+    public void testZSetOperation() {
+        Jedis jedis = jedisPool.getResource();
+
+        // 1.zadd, zcard
+
+        // 2.zscore
+
+        // 3.zincrby
+
+        // 4. zrange
+
+        // 5. zrangeByScore
+
+        // 6. zrangeByScoreWithScores
+
+        // 7. zrank
+
+        // 8. zrevrank
+
+        // 9. zrem
+
+        // 10. zrange
+
+        // 11. zrangeWithScores
+
+        // 12. zcount
+
+        // 13. 集合类操作：
+        // 13.1 zdiff, zdiffstore
+        // 13.2 zinter, zinterstore
+        // 13.3 zunion, zunionstore
+        //
+    }
 
     @AfterAll
     public static void tearDown() {
@@ -14,5 +51,6 @@ public class ZSetOperationTest extends RedisBasicTest {
         jedis.del("key3");
         jedis.del("key4");
         jedis.del("key5");
+        jedis.del("key6");
     }
 }
