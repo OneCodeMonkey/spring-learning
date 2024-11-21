ecd13d932c00eae0fda4253a7e2d85a6c02a3e04
OneCodeMonkey <1460018362@qq.com>
2024-11-21 20:03:13 +0800

    feat: websocket

---
+++ websocket/src/main/java/com/liuyang1/websocket/Greeting.java
@@ -0,0 +1,18 @@
+package com.liuyang1.websocket;
+
+public class Greeting {
+
+	private String content;
+
+	public Greeting() {
+	}
+
+	public Greeting(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
