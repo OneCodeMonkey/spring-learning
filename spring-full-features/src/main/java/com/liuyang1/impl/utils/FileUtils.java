706793743bee95c8f0a113a5cc93cf7f12f25af4
OneCodeMonkey <1460018362@qq.com>
2025-06-11 20:28:19 +0800

    feat: FileUtils

---
+++ spring-full-features/src/main/java/com/liuyang1/impl/utils/FileUtils.java
@@ -0,0 +1,22 @@
+package com.liuyang1.impl.utils;
+
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.InputStream;
+
+/**
+ * 常用文件操作
+ */
+public class FileUtils {
+    public static String readResourceFile(String filePath) {
+        ClassPathResource resource = new ClassPathResource(filePath);
+        try (InputStream inStream = resource.getInputStream()) {
+            byte[] content = inStream.readAllBytes();
+            return new String(content);
+        } catch (Exception e) {
+            System.out.println("FileUtils exception: " + e.getMessage());
+        }
+
+        return null;
+    }
+}
