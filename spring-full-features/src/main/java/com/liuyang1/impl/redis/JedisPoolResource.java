0c3552ebe6cca0ab767f760d5b860e12fa00cc33
OneCodeMonkey <1460018362@qq.com>
2025-09-17 15:50:44 +0800

    feat: Jedis redis pool

---
+++ spring-full-features/src/main/java/com/liuyang1/impl/redis/JedisPoolResource.java
@@ -0,0 +1,43 @@
+package com.liuyang1.impl.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+
+/**
+ * Author：didi
+ * Date: 6/11/24 16:26
+ * Desc:
+ */
+@Slf4j
+@Component
+public class JedisPoolResource {
+    JedisPool jedisPool;
+
+    private final static String redisIp = "127.0.0.1";
+    private final static int redisPort = 6379;
+    private final static String redisPassword = "your-password";
+    private final static int redisTimeout = 3000;
+
+    @PostConstruct
+    public void init() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(20);
+        jedisPoolConfig.setMaxIdle(200);
+        jedisPoolConfig.setMaxWait(Duration.ofMillis(3000));
+        jedisPoolConfig.setTestOnBorrow(true);
+        jedisPoolConfig.setTestOnReturn(true);
+
+        String connectionString = String.format("redis://:%s@%s:%d", redisPassword, redisIp, redisPort);
+        jedisPool = new JedisPool(jedisPoolConfig, connectionString);
+//        jedisPool = new JedisPool(jedisPoolConfig, redisIp, redisPort, redisTimeout, "", redisPassword);
+    }
+
+    public JedisPool getPoolConnection() {
+        return jedisPool;
+    }
+}
